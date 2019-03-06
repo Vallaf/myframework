a218c8b4569157a3488cf12776fe74917d736b3d
vl <valerie.lafaiteur@le-campus-numerique.fr>
2019-03-06 16:10:25 +0100

    commit class request

---
+++ core/Request.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur
+ * Date: 06/03/2019
+ * Time: 13:38
+ */
+
+namespace Core;
+
+
+class Request
+{
+
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur
+ * Date: 06/03/2019
+ * Time: 13:37
+ */
