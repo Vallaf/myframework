c5708755f8ad60c3276e70170ebdf915cd9834b9
vl <valerie.lafaiteur@le-campus-numerique.fr>
2019-03-07 14:19:24 +0100

    commit etape 4

--- public/index.php
+++ public/index.php
@@ -1,7 +1,20 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
 use \Core\Request;
+use \Core\Router\Router;
+use \Core\Router\Route;
 $request = Request::createFromGlobals();
-var_dump($request);
-//var_dump affiche les informations d'une varaible//
-die();
+$router = new Router($request);
+
+$router
+    ->addRoute(new Route("testsFoo", "/tests/foo", [], \App\Controller\TestsController::class, "foo"))
+    ->addRoute(new Route("testsBar", "/test/bar/:param", ["param" => "[\w]+"], \App\Controller\TestsController::class, "bar"));
+try {
+    $route = $router->getRouteByRequest();
+    $route->call();
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+//var_dump($request);
+//var_dump affiche les informations d'une variable//
+//die();
---
+++ src/Controller/TestsController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+
+class TestsController extends AppController
+{
+    public function foo()
+    {
+        echo 'Hello world !';
+    }
+    public function bar($bar)
+    {
+        echo $bar;
+    }
+
+
+}
